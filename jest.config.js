37ed4de875acf67ed104020bdf45cad5986ddc87
Maxim Shcherbachenko <maksym.shcherbachenko@intela.io>
2025-08-22 11:48:18 +0300

    Sumac release

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const { createConfig } = require('@openedx/frontend-build');
+
+module.exports = createConfig('jest', {
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setupTest.js',
+  ],
+  testTimeout: 20000,
+});
